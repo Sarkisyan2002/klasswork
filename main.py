f39cbb2b571b859a40cb87271bee394a9fecfffc
Sarkisyan2002 <164077225+Sarkisyan2002@users.noreply.github.com>
2024-03-20 21:57:33 +0300

    Практическая работа №2 20.02.2024

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+#coding: utf-8
+#Задача №1
+x = int(input("Введете x = "))
+y = 7 * x + 5
+print("y = " , y)
+
+#Задача №2
+а = int(input("Введете а ="))
+P = 4 * a
+print("P = " , P)
+
+#Задача №3
+r = int(input("Введете r = "))
+p = 3.14
+L = 2 * p * r
+print("L = " , L)
+
+#Задача №4
+d = int(input("Введете d = "))
+p = 3.14
+S = 1/4 * d ** 2 * p
+print("S = " , S)
+
+#Задача №5
+a = int(input("Введете a = "))
+b = int(input("Введете b = "))
+c = (a ** 2 + b ** 2) ** 0.5
+print("с = " , c)
