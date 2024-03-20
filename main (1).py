cafc8d37ba399346fdb671c08b53ea103e8b4cc0
Sarkisyan2002 <164077225+Sarkisyan2002@users.noreply.github.com>
2024-03-20 22:06:59 +0300

    Практическая работа №4 27.02.2024

---
+++ main (1).py
@@ -0,0 +1,45 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+#coding: utf-8
+#Задача №6
+a = int(input("Введите a = "))
+b =int(input("Введите b = "))
+P = a + b + ((a ** 2 + b ** 2) ** 0.5)
+print ("P = " , P)
+
+#Задача №7
+c = int(input("Введите с = "))
+f = int(input("Введите f = "))
+d = ((c ** 2 + f ** 2) ** 0.5)
+N = 2 * (c + f)
+print ("d = " , d)
+print ("N = " , N)
+
+#Задача №8
+r = int(input("Введите r = "))
+R = int(input("Введите R = "))
+pi = 3.14
+S = pi * (R ** 2 - r ** 2)
+print ("S = " , s)
+
+#Задача №9
+n = int(input("Введите n = "))
+s = 6 * n ** 2
+V = n ** 3
+print ("s = " , s)
+print ("V = " , V)
+
+#Задача №10
+i = int(input("Введите i = "))
+h = int(input("Введите h = "))
+g = int(input("Введите g = "))
+D = ((h ** 2) + ((g - i) /2) ** 2) ** 0.5
+M = i + g + 2 * D
+print ("M = " , M)
