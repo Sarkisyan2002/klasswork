b7bef5047eca5787ad215a35d347f726d5df2e02
Sarkisyan2002 <164077225+Sarkisyan2002@users.noreply.github.com>
2024-03-20 22:08:51 +0300

    Практическая работа №5 28.02.2024

---
+++ main (2).py
@@ -0,0 +1,49 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+#Задача №11
+a = int(input("Введите a = "))
+b = int(input("Введите b = "))
+Pl = a/b 
+print ("Pl = " , Pl)
+
+#Задача №12
+c = int(input("Введите с = "))
+d = int(input("Введите d = "))
+i = c + d
+f = c - d
+g = c * d
+u = c/d
+print ("i = " , i)
+print ("f = " , f)
+print ("g = " , g)
+print ("u = " , u)
+
+#Задача №13
+x = int(input("Введите x = "))
+y = int(input("Введите y = "))
+N = (x + y) /2
+M = (x * y) ** 0.5
+print ("N = " , N)
+print ("M = ", M)
+
+#Задача №14
+z = int(input("Введите z = "))
+k = int(input("Введите k = "))
+Pt = z/k
+print ("Pt = " , Pt)
+
+#Задача №15
+s = int(input("Введите s = "))
+r = int(input("Введите r = "))
+v = int(input("Введите v = "))
+Q = 2 *(s * r + s * v + r * v)
+J = s * r * v
+print ("Q = " , Q)
+print ("J = " , J)
