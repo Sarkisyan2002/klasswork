03a802c39891d2ba9fe2d0112c036623ec7bf1f9
Sarkisyan2002 <164077225+Sarkisyan2002@users.noreply.github.com>
2024-03-29 22:23:56 +0300

    Практическая работа № 8 26.03.2024

--- main.py
+++ main.py
@@ -7,31 +7,71 @@ Write your code in this editor and press "Run" button to execute it.
 '''
 
 print("Hello World")
-#coding: utf-8
 #Задача №1
-x = int(input("Введете x = "))
-y = 7 * x + 5
-print("y = " , y)
-
+A = int(input("Введите число А, не превышающее по модулю  10000:  "))
+B = int(input("Введите число B, не превышающее по модулю 10000:  "))
+C = int(input("Введите число С, не превышающее по модулю 10000:  "))
+if (A % 2 == 0 & B % 2 != 0) or (A % 2 != 0 & B % 2 == 0):
+    print ("yes")
+elif (A % 2 == 0 & B % 2 !=0) or (C % 2 !=0 & C % 2 == 0):
+    print ("yes")
+elif (B % 2 == 0 & C % 2 !=0) or (B % 2 !=0 & C % 2 == 0):
+    print ("yes")
+else:
+    print ("no")
+    
 #Задача №2
-а = int(input("Введете а ="))
-P = 4 * a
-print("P = " , P)
-
+d = int(input("Введите целое число:"))
+if d > 0:
+    print(1)
+elif d < 0:
+    print (-1)
+else:
+    print (0)
+    
 #Задача №3
-r = int(input("Введете r = "))
-p = 3.14
-L = 2 * p * r
-print("L = " , L)
-
+a = int(input("Введите число а: "))
+b = int(input("Введите число b:"))
+c = int(input("Введите число с:"))
+if a > b:
+    a, b = b, a
+if b > c:
+    b, c = c, b
+if a > b:
+    a, b = b, a
+    print(a, b, c)
+    
 #Задача №4
-d = int(input("Введете d = "))
-p = 3.14
-S = 1/4 * d ** 2 * p
-print("S = " , S)
-
+A = int(input("Дано целое число :"))
+B = int(input("Дано целое число :"))
+C = int(input("Дано целое число :"))
+D = int(input("Дано целое число :"))
+F = int(input("Дано целое число :"))
+if A <= D & B <= F or A <= F & B <= D:
+    print("YES")
+else:
+    if A > B:
+        A, B = B, A
+if D > F:
+    D, F = F, D
+if A <= D & B <= F:
+    print("Yes")
+else:
+    print("NO")
+    
 #Задача №5
-a = int(input("Введете a = "))
-b = int(input("Введете b = "))
-c = (a ** 2 + b ** 2) ** 0.5
-print("с = " , c)
+
+
+M = int(input())
+N = int(input())
+K = int(input())
+P = int(input())
+X = int(input())
+if M <= P & N <= X or M <= X & N <= P:
+    print("YES")
+elif K <= P & N <= X or K <= X & N <= P:
+    print("YES")
+elif M <= P & K <= X or M <= X & N <= P:
+    print("YES")
+else:
+    print("YES")
