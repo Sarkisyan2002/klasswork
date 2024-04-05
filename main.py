5632cadb73cc4439b811a127bbdcadf4c32226eb
Sarkisyan2002 <164077225+Sarkisyan2002@users.noreply.github.com>
2024-04-05 15:03:11 +0300

    Практическая работа №9 02.04.2024

--- main.py
+++ main.py
@@ -8,70 +8,106 @@ Write your code in this editor and press "Run" button to execute it.
 
 print("Hello World")
 #Задача №1
-A = int(input("Введите число А, не превышающее по модулю  10000:  "))
-B = int(input("Введите число B, не превышающее по модулю 10000:  "))
-C = int(input("Введите число С, не превышающее по модулю 10000:  "))
-if (A % 2 == 0 & B % 2 != 0) or (A % 2 != 0 & B % 2 == 0):
-    print ("yes")
-elif (A % 2 == 0 & B % 2 !=0) or (C % 2 !=0 & C % 2 == 0):
-    print ("yes")
-elif (B % 2 == 0 & C % 2 !=0) or (B % 2 !=0 & C % 2 == 0):
-    print ("yes")
+x = 10
+y = 3
+z = 13
+if x + y==z:
+    print("Ответ верный:")
 else:
-    print ("no")
+    print("Ответ неверный:")
     
 #Задача №2
-d = int(input("Введите целое число:"))
-if d > 0:
-    print(1)
-elif d < 0:
-    print (-1)
+num = int(input("Какое число я загадал?"))
+if num == 5:
+    print("Угадал!")
 else:
-    print (0)
+    print("Не угадал!")
+    print("Конец игры")
+if num != 6:
+    print("Угадал!")
+else:
+    print("Не угадал!")
+    print("Конец игры")
     
 #Задача №3
-a = int(input("Введите число а: "))
-b = int(input("Введите число b:"))
-c = int(input("Введите число с:"))
-if a > b:
-    a, b = b, a
-if b > c:
-    b, c = c, b
-if a > b:
-    a, b = b, a
-    print(a, b, c)
+rain = int(input("На улице идёт дождь? (Введите 1 если да, 0 если нет):"))
+if rain == 1:
+    print("Пошёл дождь.Возьмите зонт!")
+else:
+    print("Дождя нет.Можно выйти на улицу без зонта.")
     
 #Задача №4
-A = int(input("Дано целое число :"))
-B = int(input("Дано целое число :"))
-C = int(input("Дано целое число :"))
-D = int(input("Дано целое число :"))
-F = int(input("Дано целое число :"))
-if A <= D & B <= F or A <= F & B <= D:
-    print("YES")
+russian = int(input("Введите количество баллов по русскому языку: "))
+math = int(input("Введите количество баллов по математике: "))
+informatics = int(input("Введите количество баллов по информатике: "))
+total_score = russian + math + informatics
+if total_score >= 270:
+    print("Поздравляю, ты поступил на бюджет!")
 else:
-    if A > B:
-        A, B = B, A
-if D > F:
-    D, F = F, D
-if A <= D & B <= F:
-    print("Yes")
-else:
-    print("NO")
+    print("К сожалению, ты не прошёл на бюджет.")
     
 #Задача №5
-
-
-M = int(input())
-N = int(input())
-K = int(input())
-P = int(input())
-X = int(input())
-if M <= P & N <= X or M <= X & N <= P:
-    print("YES")
-elif K <= P & N <= X or K <= X & N <= P:
-    print("YES")
-elif M <= P & K <= X or M <= X & N <= P:
-    print("YES")
+number = int(input("Введите число: "))
+if number % 2 == 0:
+    print("Это чётное число. Не забудь использовать зубную нить сегодня!")
 else:
-    print("YES")
+    print("Это нечётное число. Сегодня можно отдохнуть от зубной нити.")
+    
+#Задача №6
+price_1 = float(input("Введите стоимость первого стула: "))
+price_2 = float(input("Введите стоимость второго стула:" ))
+price_3 = float(input("Введите стоимость третьего стула: "))
+total_price = price_1 + price_2 + price_3
+if total_price > 10000:
+    discount = total_price * 0.10
+    final_price = total_price - discount
+else:
+    final_price = total_price
+    print(f"Итоговая сумма чека: {final_price} рублей")
+    
+#Задача №7
+num = int(input("Введите число: "))
+if num < 0:
+    num = -num
+    print("Модуль чила:", num)
+    
+#Задача №8
+kostya_roll = int(input("Кубик Кости: "))
+owner_roll = int(input("Кубик владельца: "))
+if kostya_roll >= owner_roll:
+    difference = kostya_roll - owner_roll
+    print ("Сумма: {difference}")
+    print ("Костя платит")
+else:
+    total = kostya_roll + owner_roll
+print ("Сумма: {total}")
+print ("Владелец платит")
+print ("Игра окончена")
+
+#Задача №9
+input_amount = int(input("Введите сумму, которую хотите снять: "))
+if input_amount % 100 != 0:
+    print("Такую сумму снять невозможно. Обратитесь в другой банкомат.")
+else:
+    print("Вы можете снять указанную сумму.")
+    
+    
+worked_hours = int(input("Введите отработанные часы: "))
+remaining_loan = float(input("Введите остаток по кредиту: "))
+food_expenses = float(input("Введите траты на еду: "))
+salary = worked_hours * 200
+if salary >= remaining_loan + food_expenses:
+    print("Часов хватает. Можно отдохнуть")
+else:
+    print("Часов не хватает. Придётся работать!")
+    
+#Задача №10
+mileage = int(input("Введите пробег: "))
+day = int(input("Введите сегдняшнее число: "))
+sum_digits = sum(int(digit) for digit in str(mileage))
+if sum_digits > day:
+    print("Сброс.")
+    mileage = 0
+else:
+    print("Сегодня не сломался.")
+    print("Пробег:", mileage)
