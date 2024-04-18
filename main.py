10bcf7727818a195b8a1215c8c473a749be8560c
Sarkisyan2002 <164077225+Sarkisyan2002@users.noreply.github.com>
2024-04-18 21:53:10 +0300

    Практическая работа №12 16.04.2024

--- main.py
+++ main.py
@@ -7,114 +7,60 @@ Write your code in this editor and press "Run" button to execute it.
 '''
 
 print("Hello World")
-#Задача №6
-a = int(input("Введите число a:"))
-b = int(input("Введите число b:"))
-if a > b:
-    print(b)
+#Задача №1
+color1 = input("Введите 1 цвет: ")
+color2 = input("Введите 2 цвет: ")
+
+if (color1 == "красный" and color2 == "синий") or (color1 == "синий" and color2 == "красный"):
+    print("фиолетовый")
+elif (color1 == "красный" and color2 == "жёлтый") or (color1 == "жёлтый" and color2 == "красный"):
+    print("оранжевый")
+elif (color1 == "синий" and color2 == "жёлтый") or (color1 == "жёлтый" and color2 == "синий"):
+    print("зелёный")
+elif color1 == "красный" and color2 == "красный" :
+    print("красный")
+elif color1 == "синий" and color2 == "синий" :
+    print("синий")
+elif color1 == "жёлтый" and color2 == "жёлтый" :
+    print("жёлтый")
 else:
+    print("Ошибка цвета")
+    
+#Задача №2
+a1 = int(input("Введите целое число a1:"))
+b1 = int(input("Введите целое число b1:"))
+a2 = int(input("Введите целое число a2:"))
+b2 = int(input("Введите целое число b2:"))
+a1 = max(a1, a2)
+b1 = min(b1, b2)
+if a1 < b1:
+    print(a1,b1)
+elif a1 == b1:
     print(a)
+else:
+    print("Пустое множество")
     
-#Задача №7
-age = int(input("Введите свой возраст:"))
-if age <= 13:
-    print("детство")
-elif 14 <= age <= 24:
-    print("молодость")
-elif 25 <= age <= 59:
-    print("зрелость")
-else:
-    print("старость")
-#Задача №8
-m1 = int(input("Введите первое число:"))
-n1 = int(input("Введите второе число:"))
-c1 = int(input("Введите третье число:"))
-if m1 > 0:
-    m2 = m1
-else:
-    m2 = 0
-if n1 > 0:
-    n2 = n1
-else:
-    n2 = 0
-if c1 > 0:
-    c2 = c1
-else:
-    c2 = 0
+#Задача №3
+N = int(input("Введите четырёхзначное число N:"))
+sum = 0
+while N != 0:
+    p = N % 10
+    sum = sum + p
+    N = N // 10
+    print(sum)
     
-    print(m2 + n2 + c2)
-    
-#Задача №9
-x = int(input("Введите число x:"))
+#Задача №4
+k = int(input("Введите значение катета k:"))
+l = int(input("Введите значение катета l:"))
+c = (k ** 2 + l ** 2) ** 0.5
+print (c)
 
-if (1000 <= x <= 9999) & (x % 7 == 0 or x % 17 == 0):
+#Задача №5
+x1 = int(input("Вести координату x1:"))
+y1 = int(input("Вести координату y1:"))
+x2 = int(input("Вести координату x2:"))
+y2 = int(input("Вести координату y2:"))
+if x1 == x2 or y1 == y2:
     print("YES")
 else:
     print("NO")
-    
-#Задача №10
-i = int(input("Введите номер года:"))
-
-if (i % 4 == 0 or i % 400 == 0) & i % 100 != 0:
-    print("YES")
-else:
-    print("NO")
-    
-#Задача №11
-a1 = int(input("Введите число a1:"))
-b1 = int(input("Введите число b1:"))
-l1 = int(input("Введите число l1:"))
-if a1 == b1 == l1:
-    print("равносторонний")
-elif (a1 == b1 or a1 == l1 or b1 == l1) & (a1 !=b1 or a1 !=l1 or b1 !=l1):
-    print("равнобедренный")
-elif a1 != b1 & a1 !=l1 & b1 != l1:
-    print("равносторонний")
-    
-#Задача №12
-a2 = int(input("Введите число a2:"))
-b2 = int(input("Введите число b2:"))
-l2 = int(input("Введите число l2:"))
-if a2 < b2 < l2 or a2 > b2 > l2:
-    print(b2)
-elif b2 < l2 < a2 or b2 > l2 > a2:
-    print(l2)
-else:
-    print(a2)
-    
-#Задача №13
-y = int(input("Введите порядковый номер месяца y:"))
-
-if y == 2:
-    print(28)
-elif (x < 8 & x % 2 == 0) or (x > 7 & x % 2 != 0):
-    print(30)
-else:
-    print(31)
-    
-#Задача №14
-k = int(input("Введите вес боксёра-любителя k:"))
-if k <= 59:
-    print("Лёгкий вес")
-elif k>=60 & k<=63:
-    print("Первый полусредний вес")
-elif k>=64 & k<=68:
-    print("Полусредний вес")
-    
-#Задача №15
-g = int(input("Введите целое число g:"))
-j = int(input("Введите целое число j:"))
-s = int(input("Введите строку:"))
-if(j == 0 & s == '/'):
-    print("На ноль делить нельзя!")
-elif(s == '+'):
-    print (g+j)
-elif(s == '*'):
-    print (g*j)
-elif(s == '/'):
-    print (g/j)
-elif(s == '-'):
-    print (g-j)
-else:
-    print("Неверная операция")
-    # TODO: write code...
