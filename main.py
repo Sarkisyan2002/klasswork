b741b7dd652a221b2561d4d1089e1be90f2908a2
Sarkisyan2002 <164077225+Sarkisyan2002@users.noreply.github.com>
2024-04-30 15:26:57 +0300

    Практическая работа №13 23.04.2024

--- main.py
+++ main.py
@@ -7,60 +7,68 @@ Write your code in this editor and press "Run" button to execute it.
 '''
 
 print("Hello World")
-#Задача №1
-color1 = input("Введите 1 цвет: ")
-color2 = input("Введите 2 цвет: ")
-
-if (color1 == "красный" and color2 == "синий") or (color1 == "синий" and color2 == "красный"):
-    print("фиолетовый")
-elif (color1 == "красный" and color2 == "жёлтый") or (color1 == "жёлтый" and color2 == "красный"):
-    print("оранжевый")
-elif (color1 == "синий" and color2 == "жёлтый") or (color1 == "жёлтый" and color2 == "синий"):
-    print("зелёный")
-elif color1 == "красный" and color2 == "красный" :
-    print("красный")
-elif color1 == "синий" and color2 == "синий" :
-    print("синий")
-elif color1 == "жёлтый" and color2 == "жёлтый" :
-    print("жёлтый")
-else:
-    print("Ошибка цвета")
+#Задача №6
+a = int(input("Введите длину стороны a:"))
+b = int(input("Введите длину стороны b:"))
+c = int(input("Введите длину стороны c:"))
+if a == b == c:
+    print("равносторонний")
+elif (a == b or a == c or b == c) and (a !=b or a !=c or b !=c):
+    print("равнобедренный")
+elif a !=b and a !=c and b !=c:
+    print("разносторонний")
     
-#Задача №2
-a1 = int(input("Введите целое число a1:"))
-b1 = int(input("Введите целое число b1:"))
-a2 = int(input("Введите целое число a2:"))
-b2 = int(input("Введите целое число b2:"))
-a1 = max(a1, a2)
-b1 = min(b1, b2)
-if a1 < b1:
-    print(a1,b1)
-elif a1 == b1:
-    print(a)
-else:
-    print("Пустое множество")
+#Задача №7
+print("Координаты точки: ")
+x = int(input("x = "))
+y = int(input("y = "))
+if x > 0 and y > 0:
+    print("точка в первой четверти")
+elif x < 0 and y > 0:
+    print("точка во второй четверти")
+elif x < 0 and y < 0:
+    print("точка в третьей четверти")
+elif x > 0 and y < 0:
+    print("точка в четвёртой четверти")
+elif x == 0 and y == 0:
+    print("точка в центре координат")
+elif x == 0:
+    print("точка на оси x")
+elif y == 0:
+    print("точка на оси y")
     
-#Задача №3
-N = int(input("Введите четырёхзначное число N:"))
-sum = 0
-while N != 0:
-    p = N % 10
-    sum = sum + p
-    N = N // 10
-    print(sum)
-    
-#Задача №4
-k = int(input("Введите значение катета k:"))
-l = int(input("Введите значение катета l:"))
-c = (k ** 2 + l ** 2) ** 0.5
-print (c)
-
-#Задача №5
-x1 = int(input("Вести координату x1:"))
-y1 = int(input("Вести координату y1:"))
-x2 = int(input("Вести координату x2:"))
-y2 = int(input("Вести координату y2:"))
-if x1 == x2 or y1 == y2:
-    print("YES")
+#Задача №8
+a = int(input("Введите номер месяца a = :"))
+if a == 12 or a == 1 or a == 2:
+    print('winter')
+elif a == 3 or a == 4 or a == 5:
+    print('spring')
+elif a == 6 or a == 7 or a == 8:
+    print('sammer')
+elif a == 9 or a == 10 or a == 11:
+    print('autumn')
 else:
-    print("NO")
+    print("Error")
+    
+#Задача №9
+temperature = int(input("Введите температуру: "))
+if temperature >= 35:
+    print ("На улице очень жарко, рекомендуется под солнцем не стоять и пить побольше воды")
+elif temperature >= 20:
+    print ("На улице тепло, можно надеть шорты и футболку")
+elif temperature >= 10:
+    print ("На улице прохладно, лучше надеть джинсы и толстовку")
+else:
+    print ("На улице холодно, надо одеть куртку и шапку")
+    
+#Задача №10
+x = int(input("Введите координату x точки: "))
+y = int(input("Введите координату y точки:"))
+x1 = int(input("Введите координату x1 верхнего левого угла прямоугольника:"))
+y1 = int(input("Введите координату y1 верхнего левого угла прямоугольника:"))
+x2 = int(input("Введите координату x2 нижнего правого угла прямоугольника:"))
+y2 = int(input("Введите координату y2 нижнего правого угла прямоугольника:"))
+if x1 <= x <= x2 and y1 <= y <= y2:
+    print("Точка находится внутри прямоугольника")
+else:
+    print("Точка находится вне прямоугольника")
